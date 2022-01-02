8138365a195c0c88a5680ac30d3ee5188056acc5
Deepak from Ubuntu <deepak.pai@grynn.in>
2022-01-02 20:04:45 +0100

    web pages test.js

--- checklist/www/test/index.py
+++ checklist/www/test/index.py
@@ -2,7 +2,6 @@
 import frappe
 
 def get_context(context):
-
     context.about_us = "Hello from Python"
     context.people = ["deepak", "emm", "ant-man"]
     # context.sales_order = frappe.get_list("Sales Order", pluck=["name","grand_totals"])
